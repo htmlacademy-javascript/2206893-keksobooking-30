218c591907c49396763a27624b63653a1bee55a1
tanya.sille <tanya.sille@gmail.com>
2023-12-12 22:12:16 +0300

    добавляет изменения состояния формы и фильтра

---
+++ js/form/set-form-state.js
@@ -0,0 +1,26 @@
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+const adFormElements = adForm.childNodes;
+const mapFiltersElements = mapFilters.childNodes;
+
+const setElementsState = (elements, state) => {
+  elements.forEach((element) => {
+    element.disabled = state;
+  });
+};
+
+function activateForm () {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+  setElementsState(adFormElements, false);
+  setElementsState(mapFiltersElements, false);
+}
+
+function deactivateForm () {
+  adForm.classList.add('ad-form--disabled');
+  mapFilters.classList.add('map__filters--disabled');
+  setElementsState(adFormElements, true);
+  setElementsState(mapFiltersElements, true);
+}
+
+export {activateForm, deactivateForm};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import {renderAds} from './render-ads/render-data.js';
+import {activateForm, deactivateForm} from './form/set-form-state.js';
 
 renderAds();
+deactivateForm();
+activateForm();
